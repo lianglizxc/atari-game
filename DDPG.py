bdf130e608ab4c5f959adfbe919e3816766eadc1
Liang <li.liang@sap.com>
2019-02-25 18:02:41 +0800

    Li Liang

--- DDPG.py
+++ DDPG.py
@@ -2,9 +2,7 @@
 Deep Deterministic Policy Gradient (DDPG), Reinforcement Learning.
 DDPG is Actor Critic based algorithm.
 Pendulum example.
-
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-
 Using:
 tensorflow 1.0
 gym 0.8.0
@@ -13,21 +11,29 @@ gym 0.8.0
 import tensorflow as tf
 import numpy as np
 import gym
-import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 import time
 
 start = time.clock()
+MAX_EPISODES = 500
+MAX_EP_STEPS = 200
 
+RENDER = False
+ENV_NAME = 'Pendulum-v0'
 
 class DDPG(object):
 
-    def __init__(self, a_dim, s_dim, a_bound, lr_a = 1e-3, lr_q = 1e-3, gamma = 0.9, tau = 0.01, MEMORY_CAPACITY = 10000):
+    def __init__(self, a_dim, s_dim, a_bound, lr_a = 1e-3, lr_q = 1e-3, gamma = 0.9, tau = 0.01, var = 3,
+                 batch_size =32, MEMORY_CAPACITY = 10000):
 
-
+        self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 2), dtype=np.float32)
         self.memory_size = MEMORY_CAPACITY
         self.pointer = 0
-        self.memory = np.zeros((self.memory_size, s_dim * 2 + a_dim + 2), dtype=np.float32)
         self.a_replace_counter, self.c_replace_counter = 0, 0
+        self.var = var
+        self.batch_size = batch_size
 
         self.lr_a, self.lr_q, self.gamma, self.tau = lr_a, lr_q,gamma, tau
         self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound,
@@ -45,12 +51,12 @@ class DDPG(object):
         self.actor_para_target = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target/actor')
         self.critic_para_target = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target/critic')
 
-        q_target = self.r + gamma * q_ * (1 - self.done)
+        q_target = self.r + gamma * q_ * (1.0 - self.done)
         # in the feed_dic for the td_error, the self.a should change to actions in memory
         self.td_error = tf.losses.mean_squared_error(labels=q_target, predictions=self.q)
         self.train_op_q = tf.train.AdamOptimizer(self.lr_q).minimize(self.td_error, var_list=self.critic_para)
 
-        a_loss = - tf.reduce_mean(self.q)
+        a_loss = - tf.reduce_mean(self.q)    # maximize the q
         self.train_op_a = tf.train.AdamOptimizer(self.lr_a).minimize(a_loss, var_list=self.actor_para)
 
         self.soft_replace = [tf.assign(target, var) for target, var in zip(self.actor_para_target, self.actor_para)] + \
@@ -59,25 +65,23 @@ class DDPG(object):
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
 
-    def sample_trajectory(self):
-        pass
-
-    def _sample_action(self, s, var):
+    def sample_action(self, s):
         a = self.sess.run(self.a, feed_dict={self.s: s[None, :]})[0]
-        a = np.clip(np.random.normal(a, var), -2, 2)
+        a = np.clip(np.random.normal(a, self.var), -2, 2)
         return a
+
 
     def learn(self):
         # soft target replacement
         self.sess.run(self.soft_replace)
 
-        indices = np.random.choice(self.memory_size, size=BATCH_SIZE)
+        indices = np.random.choice(self.memory_size, size=self.batch_size)
         bt = self.memory[indices, :]
         s = bt[:, :self.s_dim]
         a = bt[:, self.s_dim: self.s_dim + self.a_dim]
-        r = bt[:, self.s_dim + self.a_dim: self.s_dim + self.a_dim + 1]
-        s_next = bt[:, -self.s_dim-1: -1]
-        done = bt[:,[-1]]
+        r = bt[:, -self.s_dim - 1: -self.s_dim]
+        s_next = bt[:, -self.s_dim-1:-1]
+        done = bt[:, [-1]]
 
         self.sess.run(self.train_op_a, {self.s: s})
         _ , lose = self.sess.run([self.train_op_q, self.td_error], {self.s: s, self.a: a,
@@ -114,7 +118,7 @@ class DDPG(object):
             return self.a_bound * a
 
     def _critic_net(self, s, a, trainable):
-        hidden_units = [40, 20, 10]
+        hidden_units = [40, 20]
         activation = [tf.nn.relu, tf.nn.relu]
         layer = tf.concat([s, a], axis=1)
         with tf.variable_scope('critic'):
@@ -127,58 +131,55 @@ class DDPG(object):
                                 kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)
             return q
 
+###############################  training  ####################################
 
-if __name__ == '__main__':
-    MAX_EPISODES = 500
-    MAX_EP_STEPS = 200
-    BATCH_SIZE = 32
+env = gym.make(ENV_NAME)
+env = env.unwrapped
+env.seed(1)
 
-    RENDER = False
-    ENV_NAME = 'Pendulum-v0'
-    env = gym.make(ENV_NAME)
-    env = env.unwrapped
-    env.seed(1)
+s_dim = env.observation_space.shape[0]
+a_dim = env.action_space.shape[0]
+a_bound = env.action_space.high
 
-    s_dim = env.observation_space.shape[0]
-    a_dim = env.action_space.shape[0]
-    a_bound = env.action_space.high
+ddpg = DDPG(a_dim, s_dim, a_bound)
 
-    ddpg = DDPG(a_dim, s_dim, a_bound)
+episode_reward = []
+for i in range(MAX_EPISODES):
+    s = env.reset()
+    ep_reward = 0
+    if ddpg.var >= 0.1:
+        ddpg.var *= .985  # decay the action randomness
 
-    var = 3  # control exploration
-    episode_reward = []
+    for j in range(MAX_EP_STEPS):
+        if RENDER:
+            env.render()
 
-    for i in range(MAX_EPISODES+1):
-        s = env.reset()
-        ep_reward = 0
+        # Add exploration noise
+        a = ddpg.sample_action(s)  # add randomness to action selection for exploration
+        s_, r, done, info = env.step(a)
 
-        for j in range(MAX_EP_STEPS):
-            if RENDER:
-                env.render()
+        ddpg.store_transition(s, a, r, s_, done)
 
-            if var >= 0.01:
-                var *= 0.98
+        if ddpg.pointer > ddpg.memory_size:
+            ddpg.learn()
 
-            a = ddpg.sample_action(s, var)
-            s_, r, done, info = env.step(a)
+        s = s_
+        ep_reward += r
+        if done: break
 
-            ddpg.store_transition(s, a, r, s_, done)
+    episode_reward.append(ep_reward)
+    print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % ddpg.var, )
+    if ep_reward > 10:
+        RENDER = True
 
-            if ddpg.pointer > ddpg.memory_size:
-                ddpg.learn()
+end = time.clock()
+print('Running time: %s Seconds' % (end - start))
 
-            s = s_
-            ep_reward += r
-            if j == MAX_EP_STEPS-1 or done:
-                episode_reward.append((i,ep_reward))
+sns.set(style="darkgrid")
+plt.figure(1)
+plt.plot(episode_reward, label='DDPG')
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.legend(loc='best')
 
-                if i % 10 == 0:
-                    print('Episode:', i, ' Reward: %i' % ep_reward, ' exploration: %.2f' % var)
-
-                if ep_reward > 10:RENDER = True
-                break
-
-    end = time.clock()
-    print('Running time: %s Seconds' % (end - start))
-    filename = ENV_NAME + '_experiment_%s' % 1
-    pd.DataFrame(episode_reward, columns = ['episode','reward']).to_csv(filename,index= False)
+plt.show()
